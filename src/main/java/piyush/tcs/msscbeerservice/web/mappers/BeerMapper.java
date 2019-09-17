575fcb43ad13811ce6da1eb261ce4e4aba8764c4
Piyush Srivastava <piyush061996@gmail.com>
2019-09-17 19:38:54 +0530

    adding mapstruct

---
+++ src/main/java/piyush/tcs/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,12 @@
+package piyush.tcs.msscbeerservice.web.mappers;
+
+import org.mapstruct.Mapper;
+import piyush.tcs.msscbeerservice.domain.Beer;
+import piyush.tcs.msscbeerservice.web.model.BeerDto;
+
+@Mapper(uses= DateMapper.class)
+public interface BeerMapper {
+
+    BeerDto BeerToBeerDto(Beer beer);
+    Beer BeerDtoToBeer(BeerDto dto);
+}
